4d8b7bce1e9079b4bf6682d3e6a7d3daca982b63
marcoschultznunes <marco_sn@hotmail.com>
2022-06-15 16:15:26 -0300

    Comments, commas, & CSS improvements

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,21 @@ import React, { useState } from "react";
 import UserForm from "./components/form/UserForm";
 import UsersList from "./components/list/UsersList";
 
+/*
+    App consists of:
+
+    An UserForm, which adds users to the users list
+    An UserList, which displays the list of users
+*/
 const App = () => {
-    const [users, setUsers] = useState([])
+    const [users, setUsers] = useState([]);
 
     const addUser = (username, age) => {
         setUsers(prevUsers => [...prevUsers, {
             username: username,
             age: age
-        }])
-    }
+        }]);
+    };
 
     return <React.Fragment>
         <UserForm addUser={addUser}/>
--- src/components/form/UserForm.jsx
+++ src/components/form/UserForm.jsx
@@ -3,22 +3,28 @@ import "./UserForm.css";
 import validate from '../validators/User';
 
 const UserForm = (props) => {
+    /* The input refs */
     const usernameRef = useRef();
     const ageRef = useRef();
+
+    /* errors: validation error messages as a list */
     const [errors, setErrors] = useState([]);
 
+    /* changeHandler: resets the errors when a value is changed */
     const changeHandler = (e) => {
-        setErrors([])
-    }
+        setErrors([]);
+    };
 
+    /* submitHandler: validates, submits, and clears the input fields */
     const submitHandler = (e) => {
-        e.preventDefault()
+        e.preventDefault();
         const username = usernameRef.current.value;
         const age = ageRef.current.value;
 
+        // validate: returns an array with an error message for each field
         const validationErrors = validate(username, age);
 
-        if(validationErrors.length === 0){
+        if(validationErrors.length === 0){  // submits if no errors 
             props.addUser(username, Number.parseInt(age));
             usernameRef.current.value = "";
             ageRef.current.value = "";
@@ -37,7 +43,7 @@ const UserForm = (props) => {
             <input type="text" name="age" ref={ageRef} onChange={changeHandler}/>
         </div>
         <div className="error-msgs-container">
-            {errors.map((e, i) => <p key={i}>* {e}</p>)}
+            {errors.map((e, i) => <p key={i}>* {e}</p>) /* Each error is displayed in a p */} 
         </div>
         <div className="submit-btn-container">
             <button type="submit">Add User</button>
